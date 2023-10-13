5d9a64de8ab4fe9968634814a1eec85bc402df68
021Coding <021coding@gmail.com>
2023-10-13 04:10:21 -0600

    Added Movement Cooldowns

--- src/collisions/Collider.ts
+++ src/collisions/Collider.ts
@@ -12,17 +12,21 @@ export class Collider {
         if(this.player.getX() < 0) {
             this.player.setX(0);
             this.player.resetXVel();
+            this.player.resetJump();
         } else if(this.player.getX() + this.player.getSize() > GAME_WIDTH) {
             this.player.setX(GAME_WIDTH - this.player.getSize());
             this.player.resetXVel();
+            this.player.resetJump();
         }
 
         if(this.player.getY() < 0) {
             this.player.setY(0);
             this.player.resetYVel();
+            this.player.resetJump();
         } else if(this.player.getY() + this.player.getSize() > GAME_HEIGHT) {
             this.player.setY(GAME_HEIGHT - this.player.getSize());
             this.player.resetYVel();
+            this.player.resetJump();
         } 
     }
 
--- src/game/player.ts
+++ src/game/player.ts
@@ -1,6 +1,7 @@
 import { MouseInput } from "../input/mouseInput";
 import { Collider } from "../collisions/Collider";
 import { KeyboardInput } from "../input/keyboardInput";
+import { GAME_HEIGHT, GAME_WIDTH } from "../utils/utils";
 
 export class Player {
     // Location and size of the player
@@ -14,6 +15,10 @@ export class Player {
     private xVel: number;
     private yVel: number; 
 
+    // Vairables for movement
+    private canJump: boolean;
+    private canBoost: boolean;
+
     constructor(startX: number, startY: number, size: number) {
         this.x = startX;
         this.y = startY;
@@ -23,6 +28,9 @@ export class Player {
 
         this.xVel = 0;
         this.yVel = 0;
+
+        this.canJump = false;
+        this.canBoost = false;
     }
 
     public update(mouseInput: MouseInput, keyboardInput: KeyboardInput) {
@@ -37,7 +45,7 @@ export class Player {
             this.xVel += 0.1;
         }
         if(keyboardInput.jumpPressed()) {
-            this.yVel -= 1;
+            this.jump();
         }
 
         // -----------------------------------
@@ -57,17 +65,37 @@ export class Player {
         ctx.fillRect(this.x, this.y, this.size, this.size);
     }
 
+    // Jump
+    private jump() {
+        if(this.canJump) {
+            if(this.y >= GAME_HEIGHT - this.size) {
+                this.yVel -= 5;
+            } else if(this.x <= 0) {
+                this.xVel += 5;
+            } else if(this.x >= GAME_WIDTH - this.size) {
+                this.xVel -= 5;
+            } else if(this.y <= 0) {
+                this.yVel += 5;
+            } else {
+                this.yVel = -5;
+            }
+
+            this.canJump = false;
+        }
+    }
+
     // Add force towards a location
     private boost(targetX: number, targetY: number) {
-        let dX: number = targetX - this.x;
-        let dY: number = targetY - this.y;
-        let theta: number = Math.atan2(dY, dX);
+        if(this.canBoost && !this.canJump) {
+            let dX: number = targetX - this.x;
+            let dY: number = targetY - this.y;
+            let theta: number = Math.atan2(dY, dX);
 
-        console.log(dX);
-        console.log(dY);
-        console.log(theta);
-        this.xVel += Math.cos(theta) * 5;
-        this.yVel += Math.sin(theta) * 5;
+            this.xVel += Math.cos(theta) * 10;
+            this.yVel += Math.sin(theta) * 10;
+
+            this.canBoost = false;
+        }
     }
 
     public getCollider() {
@@ -100,6 +128,11 @@ export class Player {
 
     public resetYVel() {
         this.yVel = 0;
+    }
+
+    public resetJump() {
+        this.canJump = true;
+        this.canBoost = true;
     }
 
 }
